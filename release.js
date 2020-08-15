0f4b6a5b94fe75054608bf2f6095ea5345a9c4b9
Richard McCartney <richard@mccartney.io>
2020-08-15 16:16:16 +0100

    ci(release): adding multi release

---
+++ release.js
@@ -0,0 +1,38 @@
+const multirelease = require('multi-semantic-release');
+const { getManifest } = require('multi-semantic-release/lib/getManifest');
+
+const dryRun = false;
+
+async function release() {
+  try {
+    const paths = [
+      `${__dirname}/browserlist-config/package.json`,
+      `${__dirname}/eslint-config/package.json`,
+      `${__dirname}/prettier-config/package.json`,
+      `${__dirname}/stylelint-config/package.json`,
+      `${__dirname}/animation/package.json`,
+      `${__dirname}/colour/package.json`,
+      `${__dirname}/components/package.json`,
+      `${__dirname}/elements/package.json`,
+      `${__dirname}/grid/package.json`,
+      `${__dirname}/icons/package.json`,
+      `${__dirname}/icons-react/package.json`,
+      `${__dirname}/icons-web-components/package.json`,
+      `${__dirname}/layout/package.json`,
+      `${__dirname}/react/package.json`,
+      `${__dirname}/themes/package.json`,
+      `${__dirname}/tokens/package.json`,
+      `${__dirname}/typography/package.json`,
+      `${__dirname}/web-components/package.json`
+    ];
+
+    await multirelease(paths, { dryRun });
+  }
+  catch (error) {
+    // Log out errors.
+    console.error('[multi-semantic-release]:', error);
+    process.exit(1);
+  }
+}
+
+release();
